658cdb54d22222937cfb5f6a4fb8594a392825d9
Qier LU <lvqier@gmail.com>
2020-06-10 17:05:13 +0800

    list: fix rendering of form shown in modal

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Fairy_Admin-0.0.4",
-    version="0.0.4",
+    name="Fairy_Admin-0.0.5",
+    version="0.0.5",
     author="Qier LU",
     author_email="lvqier@gmail.com",
     description="Fairy Admin add template based on Layui to Flask Admin",
