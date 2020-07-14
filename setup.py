09a131735eaffba49fb0e41cee7057f6c1e7883d
Qier LU <lvqier@gmail.com>
2020-07-14 11:35:44 +0800

    setup: force min python version to 3.5

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Fairy_Admin-0.0.20",
-    version="0.0.20",
+    name="Fairy_Admin-0.0.21",
+    version="0.0.21",
     author="Qier LU",
     author_email="lvqier@gmail.com",
     description="Fairy Admin add template based on Layui to Flask Admin",
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     install_requires=[
         "flask_admin",
         "flask_security",
